bc9a006b2943594665de8419a4c09cd092e4a58c
Tofu <athenavladimir@gmail.com>
2020-09-08 23:33:21 +0700

    first commit

---
+++ app/src/main/java/com/tofukma/shippingapp/model/ShipperUserModel.kt
@@ -0,0 +1,8 @@
+package com.tofukma.shippingapp.model
+
+class ShipperUserModel {
+    var uid:String ?= null
+    var name:String ?= null
+    var phone:String ?= null
+    var isActive:Boolean = false
+}
---
+++ app/src/main/java/com/tofukma/shippingapp/model/TokenModel.kt
@@ -0,0 +1,12 @@
+package com.tofukma.shippingapp.model
+
+class TokenModel {
+    var uid:String ?= null
+    var token:String ?= null
+
+    constructor()
+    constructor(uid:String,token:String){
+        this.uid = uid
+        this.token = token
+    }
+}

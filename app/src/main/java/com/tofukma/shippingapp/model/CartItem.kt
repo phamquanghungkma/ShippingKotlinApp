eb3dc26c456bab63a70d52a1b88f7269d8ee2e66
= <=>
2020-09-11 13:08:53 +0700

    done video 51

---
+++ app/src/main/java/com/tofukma/shippingapp/model/CartItem.kt
@@ -0,0 +1,24 @@
+package com.tofukma.shippingapp.model
+
+class CartItem {
+    var foodId:String =""
+
+    var foodName:String?=null
+
+    var foodImage:String?=null
+
+    var foodPrice:Double=0.0
+
+    var foodQuantity:Int=0
+
+    var foodAddon:String?=null
+
+    var foodSize:String?=null
+
+    var userPhone:String?=""
+
+    var foodExtraPrice:Double = 0.0
+
+
+    var uid:String?=""
+}
